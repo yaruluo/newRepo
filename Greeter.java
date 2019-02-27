5dced6b21c02364bf7307e55835db824d0f2590a
Yaru Luo <yluo00@stuy.edu>
2019-02-27 00:20:12 -0500

    committed

---
+++ Greeter.java
@@ -0,0 +1,28 @@
+/******************************************
+ * File name:     Greeter,java
+ * Compilation:   javac Greeter.java
+ * Execution:     java Greeter
+ *
+ * Parameter-ized to greet a particular person
+******************************************/
+
+public class Greeter {
+
+  /* It is not legal to put the greet method before the
+  main method since it is called in the main method */
+
+  public static void main(String[] commandLine){ //String is a class
+    greet("Professor Kernighan");
+    greet("Admiral Hopper");
+    greet("Good-time Johnny");
+    // It is invalid to nest methods inside of another
+  }
+  public static void greet(String person) {
+    System.out.println("hello, "  //System is a class
+                      + person
+                      + "! How you doon?");
+  }
+}
+
+  /* It is not legal to put the greet method outside the
+  Greeter class */
